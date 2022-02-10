3ed8357a218a076a29000528e5acbaa5ad0f60d5
zyx898 <zyx111901@gmail.com>
2022-02-10 14:41:36 -0600

    finished

--- project1.js
+++ project1.js
@@ -1,38 +1,90 @@
-// An highlighted block
-var a = 50;//半径
-var d = 2 * a;
+var radius = 50;//半径
+var d = 2 * radius;
 var x = 0;
 
+
 function setup() {
-  createCanvas(635, 635);
+  createCanvas(windowWidth, windowHeight);
   noStroke();
   rectMode(CENTER);
-  frameRate(30);    
+  frameRate(20);    
+  window.alert('Welcome to my Art 151 Project 1.');
+  window.alert('Just simpely drag your mouse across the page.');
+  window.alert('Or You can use your left arrow and right arrow');
+  window.alert('You can also use Key R to change color');
+  r = random(255); // r is a random number between 0 - 255
+  g = random(100,200); // g is a random number betwen 100 - 200
+  b = random(100); // b is a random number between 0 - 100
 }
 
 function draw1() {
     fill(230);
-    for(i=1;i<=6;i++){
-      for(j = 1;j<=6;j++){
-    ellipse((2*i-1)*a + 5*i, (2*j-1)*a + 5*j, d, d);
+    for(i=1;i<=(windowWidth/10);i++){
+      for(j = 1;j<=(windowHeight/10);j++){
+    ellipse((2*i-1)*radius + 5*i, (2*j-1)*radius + 5*j, d, d);
       }
     }
   }
+  
   function draw2() {
-    fill(32);
-    x+=0.1;
-    for(i=1;i<=3;i++){
-      for(j = 1;j<=3;j++){
-    arc((4*i-3)*a + 5*(2*i-1), (4*j-3)*a + 5*(2*j-1), d+1, d+1, -x, PI/2-x); 
-    arc((4*i-1)*a + 10*i, (4*j-3)*a + 5*(2*j-1), d+1, d+1, HALF_PI+x, PI/2+HALF_PI+x); 
-    arc((4*i-3)*a + 5*(2*i-1), (4*j-1)*a + 10*j, d+1, d+1, -HALF_PI+x, PI/2-HALF_PI+x); 
-    arc((4*i-1)*a + 10*i, (4*j-1)*a + 10*j, d+1, d+1, PI-x, HALF_PI+PI-x); 
+
+    // x+=0.1;
+    for(i=1;i<=(windowWidth/20);i++){
+      for(j = 1;j<=(windowHeight/20);j++){
+        fill(r, g, b);
+        arc((4*i-3)*radius + 5*(2*i-1), (4*j-3)*radius + 5*(2*j-1), d+1, d+1, -x, PI/2-x); 
+        arc((4*i-1)*radius + 10*i, (4*j-3)*radius + 5*(2*j-1), d+1, d+1, HALF_PI+x, PI/2+HALF_PI+x); 
+        arc((4*i-3)*radius + 5*(2*i-1), (4*j-1)*radius + 10*j, d+1, d+1, -HALF_PI+x, PI/2-HALF_PI+x); 
+        arc((4*i-1)*radius + 10*i, (4*j-1)*radius + 10*j, d+1, d+1, PI-x, HALF_PI+PI-x); 
       }
     }
   }
+function mouseDragged() {
+  x+=0.1;
+  if (x > 255) {
+    x = 0;
+  }
+}
+
+
+function keyPressed() {
+  if (keyCode === 82 ) {
+    r = random(255); // r is a random number between 0 - 255
+    g = random(100,200); // g is a random number betwen 100 - 200
+    b = random(100); // b is a random number between 0 - 100
+  }
+
+}
+
 
 function draw(){
   background(32);
   draw1();
   draw2();
+  if (keyIsDown(LEFT_ARROW)) {
+    x+=0.1;
+    if (x > 255) {
+      x = 0;
+    }
+  } 
+  if (keyIsDown(RIGHT_ARROW)) {
+    x-=0.1;
+    if (x > 255) {
+      x = 0;
+    }
+  }
+  if (keyIsDown(65)) {
+    x+=0.1;
+    if (x > 255) {
+      x = 0;
+    }
+  } 
+  if (keyIsDown(68)) {
+    x-=0.1;
+    if (x > 255) {
+      x = 0;
+    }
+  }
+
+
 }
